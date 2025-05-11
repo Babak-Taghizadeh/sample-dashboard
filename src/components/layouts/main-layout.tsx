28154a0656aba6c6d555ae5061958dbdd8087d09
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-11 16:57:32 +0330

    feat: apply sidebar component and provider into main-layout

--- src/components/layouts/main-layout.tsx
+++ src/components/layouts/main-layout.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./header";
 import { cn } from "@/lib/utils";
+import AppSidebar from "../section/app-sidebar";
+import { SidebarProvider } from "../ui/sidebar";
 
 interface MainLayoutProps {
   children: React.ReactNode;
@@ -9,12 +11,15 @@ interface MainLayoutProps {
 
 const MainLayout = ({ children, className }: MainLayoutProps) => {
   return (
-    <div className={cn("flex min-h-screen flex-col", className)}>
-      <Header />
-      <main className="grid flex-grow grid-cols-1 gap-4 px-4 md:grid-cols-12">
-        {children}
-      </main>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <div className={cn("flex min-h-screen w-full flex-col", className)}>
+        <Header />
+        <main className="grid flex-grow grid-cols-1 gap-4 px-4 md:grid-cols-12">
+          {children}
+        </main>
+      </div>
+    </SidebarProvider>
   );
 };
 
