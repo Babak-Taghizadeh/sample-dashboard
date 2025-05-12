2955db6a7e9b1e5eca9e674a2dcef9b212d96625
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-12 13:04:37 +0330

    feat: apply refer radar component into card

--- src/components/section/referral-card.tsx
+++ src/components/section/referral-card.tsx
@@ -9,22 +9,23 @@ import ReferRadar from "./refer-radar";
 
 const ReferralCard = () => {
   return (
-    <Card className="relative max-w-[460px] overflow-hidden">
+    <Card className="relative w-full max-w-[460px]">
       <CardHeader className="h-[320px]">
         <ReferRadar />
       </CardHeader>
 
-      <CardContent className="p-6">
-        <h3 className="text-lg font-semibold">Referral Program</h3>
-        <p className="text-muted-foreground mt-2">
-          Invite friends and earn rewards together
+      <CardContent className="flex flex-col gap-2 items-center px-10 mt-10">
+        <h3 className="text-lg font-semibold">
+          Refer friends and earn with Patara!
+        </h3>
+        <p className="text-muted-foreground text-center text-pretty">
+          Invite your friends to Patara and earn a share of their on-chain
+          rewards forever!
         </p>
       </CardContent>
 
-      <CardFooter className="p-6 pt-0">
-        <Button>
-          Connect/Sign In
-        </Button>
+      <CardFooter className="self-center">
+        <Button className="py-6">Connect/Sign In</Button>
       </CardFooter>
     </Card>
   );
