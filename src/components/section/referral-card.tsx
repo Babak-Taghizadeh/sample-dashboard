03a41b8c639e8cdf04466e82cda1f4c8778ffaac
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-12 13:47:07 +0330

    style: add tailwind media queries to fulfill responsive needs in card

--- src/components/section/referral-card.tsx
+++ src/components/section/referral-card.tsx
@@ -6,19 +6,21 @@ import {
   CardHeader,
 } from "@/components/ui/card";
 import ReferRadar from "./refer-radar";
+import GradientBackground from "../ui/gradient-background";
 
 const ReferralCard = () => {
   return (
-    <Card className="relative w-full max-w-[460px]">
-      <CardHeader className="h-[320px]">
+    <Card className="relative w-full max-w-[460px] gap-4 py-4 sm:gap-10 sm:py-10">
+      <CardHeader className="max-h-[360px]">
+        <GradientBackground height="440" />
         <ReferRadar />
       </CardHeader>
 
-      <CardContent className="flex flex-col gap-2 items-center px-10 mt-10">
-        <h3 className="text-lg font-semibold">
+      <CardContent className="flex flex-col items-center gap-2 px-6 sm:mt-6">
+        <h3 className="text-center text-xl font-semibold text-pretty sm:text-2xl">
           Refer friends and earn with Patara!
         </h3>
-        <p className="text-muted-foreground text-center text-pretty">
+        <p className="text-muted-foreground px-4 text-center text-pretty">
           Invite your friends to Patara and earn a share of their on-chain
           rewards forever!
         </p>
