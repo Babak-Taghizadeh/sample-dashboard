cd699945fabb8e09d4d11b6fcaca4094901c6ee5
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-12 22:10:11 +0330

    feat: add basic metadata for better user experience

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,14 @@
 import AnimatedAlphaDots from "@/components/section/animated-alpha-dots";
 import ReferralCard from "@/components/section/referral-card";
+import Head from "next/head";
 
 export default function Home() {
-  return <AnimatedAlphaDots Content={<ReferralCard />} />;
+  return (
+    <>
+      <Head>
+        <title>Referral & Earnings Dashboard</title>
+      </Head>
+      <AnimatedAlphaDots Content={<ReferralCard />} />
+    </>
+  );
 }
