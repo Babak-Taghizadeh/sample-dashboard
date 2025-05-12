2969a224bf630a58cdb43e681ff34e527d0ed7aa
Babak Taghizadeh <babak.tz98@gmail.com>
2025-05-12 21:43:28 +0330

    fix: show user profile in header when logged in

--- src/components/layouts/header/header-actions.tsx
+++ src/components/layouts/header/header-actions.tsx
@@ -1,10 +1,23 @@
 import { Button, buttonVariants } from "@/components/ui/button";
 import BellIcon from "../../../../public/icons/Bell.svg";
 import GearIcon from "../../../../public/icons/Gear.svg";
+import PLogo from "../../../../public/images/p-logo.png";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
 
 const HeaderActions = ({ isInSidebar }: { isInSidebar?: boolean }) => {
+  const pathname = usePathname();
+  console.log(pathname);
   return (
     <div
       className={cn(
@@ -18,15 +31,31 @@ const HeaderActions = ({ isInSidebar }: { isInSidebar?: boolean }) => {
       <Button variant="secondary" size="icon">
         <GearIcon />
       </Button>
-      <Link
-        href="/profile"
-        className={cn(
-          buttonVariants({ variant: "default", size: "default" }),
-          isInSidebar && "flex-auto",
-        )}
-      >
-        Connect/Sign In
-      </Link>
+      {pathname === "/profile" ? (
+        <Select defaultValue="user">
+          <SelectTrigger className="w-[160px]">
+            <SelectValue placeholder="Transaction Count" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="user">
+                <Image src={PLogo} alt="p-logo" width={25} height={25} />
+                <span>@patara.sui</span>
+              </SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      ) : (
+        <Link
+          href="/profile"
+          className={cn(
+            buttonVariants({ variant: "default", size: "default" }),
+            isInSidebar && "flex-auto",
+          )}
+        >
+          Connect/Sign In
+        </Link>
+      )}
     </div>
   );
 };
